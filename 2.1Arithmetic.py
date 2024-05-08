8f30dda5facf0f16c0bc492b6df72716a4098507
muchai <leemuchai30@gmail.com>
2024-05-08 13:59:49 +0300

    Arithmetic operator

---
+++ 2.1Arithmetic.py
@@ -0,0 +1,22 @@
+#Arithmetic Operators
+x = 15
+y = 7
+#Unary
+z = +x
+print(z)
+#addition
+print("Addition x + Y = ",x+y)
+#Substraction
+print("Subtraction x - Y = ",x-y)
+#multiplication
+print("Multiplication x * Y = ",x*y)
+#Division
+print("Division x / Y = ",x/y)
+#Floor - Integer no float
+print("Floor x // Y = ",x//y)
+#Modulus - Remainder
+print("Modulus x % Y = ",x%y)
+#Power
+print("Power x ** Y = ",x**y)
+
+
