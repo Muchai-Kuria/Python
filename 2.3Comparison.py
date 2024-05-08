5a1c9c3b522e6e272efb40d14aa6afe06baf6e6b
muchai <leemuchai30@gmail.com>
2024-05-08 14:33:57 +0300

    Comaprison operator

---
+++ 2.3Comparison.py
@@ -0,0 +1,10 @@
+x = "Apple"
+y = "Orange"
+print(x >= y)
+
+x = 10
+y = 20
+print("X Equal to Y: ",x == y)
+print("X Greater to or Equal to Y: ",x >= y)
+print("X Lesser to or Equal to Y: ",x <= y)
+print("X not Equal to Y: ",x != y)
