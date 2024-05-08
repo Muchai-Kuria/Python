9d1debbb3590bd7ab0e82826abd5f9925f231b06
muchai <leemuchai30@gmail.com>
2024-05-08 14:19:08 +0300

    Assignment operator

---
+++ 2.2Assignment.py
@@ -0,0 +1,18 @@
+x =10
+y =20
+
+#+=  operator is used to add and assign the value of y to x. It means that it adds the value of y to x and then assigns theprint("x is",x)print("x is",x)print(x+y) # 30
+x +=y
+print("+=",x)
+#^=  ^= means bitwise XOR assignment operator. It performs a bitwise exclusive OR operation on the variable and the value (rhs), and # 30 # 30 # 30    
+x =10 
+x ^=y 
+print (x)
+#&=  and |= operators are used for bitwise AND and OR operations respectively.
+x =10
+x&=y
+print(x)
+#>>= operator is used to shift the bits of number x to right by y positions.
+x= 10
+x >>=y
+print(x)
